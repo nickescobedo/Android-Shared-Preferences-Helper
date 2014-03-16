c5b7bd5fadb90a093e188c89c16e466890e7d6c3
Nick Escobedo <nick@nickescobedo.com>
2014-03-16 16:00:25 -0500

    Initial commit.

---
+++ SharedPreferenceHelper.java
@@ -0,0 +1,78 @@
+public class SharedPreferenceHelper {
+    private final static String PREF_FILE = "PREF";
+
+    /**
+     * Set a string shared preference
+     * @param key - Key to set shared preference
+     * @param value - Value for the key
+     */
+    static void setSharedPreferenceString(Context context, String key, String value){
+        SharedPreferences settings = context.getSharedPreferences(PREF_FILE, 0);
+        SharedPreferences.Editor editor = settings.edit();
+        editor.putString(key, value);
+        editor.commit();
+    }
+
+    /**
+     * Set a integer shared preference
+     * @param key - Key to set shared preference
+     * @param value - Value for the key
+     */
+    static void setSharedPreferenceInt(Context context, String key, int value){
+        SharedPreferences settings = context.getSharedPreferences(PREF_FILE, 0);
+        SharedPreferences.Editor editor = settings.edit();
+        editor.putInt(key, value);
+        editor.commit();
+    }
+
+    /**
+     * Set a Boolean shared preference
+     * @param key - Key to set shared preference
+     * @param value - Value for the key
+     */
+    static void setSharedPreferenceBoolean(Context context, String key, boolean value){
+        SharedPreferences settings = context.getSharedPreferences(PREF_FILE, 0);
+        SharedPreferences.Editor editor = settings.edit();
+        editor.putBoolean(key, value);
+        editor.commit();
+    }
+
+    /**
+     * Get a string shared preference
+     * @param key - Key to look up in shared preferences.
+     * @param defValue - Default value to be returned if shared preference isn't found.
+     * @return value - String containing value of the shared preference if found.
+     */
+    static String getSharedPreferenceString(Context context, String key, String defValue){
+        SharedPreferences settings = context.getSharedPreferences(PREF_FILE, 0);
+        String value = settings.getString(key, defValue);
+
+        return value;
+    }
+
+    /**
+     * Get a integer shared preference
+     * @param key - Key to look up in shared preferences.
+     * @param defValue - Default value to be returned if shared preference isn't found.
+     * @return value - String containing value of the shared preference if found.
+     */
+    static int getSharedPreferenceInt(Context context, String key, int defValue){
+        SharedPreferences settings = context.getSharedPreferences(PREF_FILE, 0);
+        int value = settings.getInt(key, defValue);
+
+        return value;
+    }
+
+    /**
+     * Get a boolean shared preference
+     * @param key - Key to look up in shared preferences.
+     * @param defValue - Default value to be returned if shared preference isn't found.
+     * @return value - String containing value of the shared preference if found.
+     */
+    static boolean getSharedPreferenceBoolean(Context context, String key, boolean defValue){
+        SharedPreferences settings = context.getSharedPreferences(PREF_FILE, 0);
+        boolean value = settings.getBoolean(key, defValue);
+
+        return value;
+    }
+}
